7278a9c9276802355e5c0a5be58b7f3558641a71
Magnus Giverin <magnusgiverin@icloud.com>
2024-10-26 14:44:06 +0200

    Add: Add analitics

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,12 @@
-import '../styles/global.css'; 
+import '../styles/global.css';
+import { Analytics } from "@vercel/analytics/react"
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <Analytics>
+      <Component {...pageProps} />;
+    </Analytics>
+  )
 }
 
 export default MyApp;
