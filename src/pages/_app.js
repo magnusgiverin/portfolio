d2f321a76cf3745ec1090d82e270163b18d59487
Magnus Giverin <magnusgiverin@icloud.com>
2024-10-26 14:47:42 +0200

    Fix: Fix analitics

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,9 +3,10 @@ import { Analytics } from "@vercel/analytics/react"
 
 function MyApp({ Component, pageProps }) {
   return (
-    <Analytics>
-      <Component {...pageProps} />;
-    </Analytics>
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+    </>   
   )
 }
 
